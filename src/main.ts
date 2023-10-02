447ffc8cea1f5867a6afaf626c10534edfb413bd
wojowoof <wojowoof@yahoo.com>
2023-10-02 10:37:59 -0700

    Forgot reactive part

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,18 @@ import 'zone.js/dist/zone';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
+
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'my-app',
   standalone: true,
-  imports: [CommonModule, FormsModule, MatFormFieldModule],
+  imports: [CommonModule, FormsModule, MatFormFieldModule, ReactiveFormsModule],
   template: `
     <h1>Colspan Issue</h1>
-    <form>
+    <form [formGroup]="testFormGroup">
     <table>
       <tr>
         <td>XXX</td><td>One</td><td>Two</td><td>Three</td><td>Four</td>
@@ -36,7 +38,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
         <td>D</td>
         <td>
         <mat-form-field>
-          <textarea matInput rows="4" cols="40">Whaddya want for nuttin'? Rubber biscuit?</textarea>
+          <textarea matInput rows="4" cols="40" formControlName=>Whaddya want for nuttin'? Rubber biscuit?</textarea>
         </mat-form-field>
         </td>
       </tr>
@@ -53,6 +55,15 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 export class App {
   name = 'ColspanIgnore';
   title = 'Colspan Issue';
+
+  testFormGroup: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.testFormGroup = this.formBuilder.group({
+      textFieldA: '',
+      textFieldB: "Whaddya want fer nuttin' - Rrrrrrruubbber Biscuit?",
+    });
+  }
 }
 
 bootstrapApplication(App);
